aae161f64a2a1ca233e4b04c3f219155443c8bd2
avivv <>
2021-05-13 14:30:47 +0300

    V2 color class colorPrint.red("sdsds")

--- colorClass.py
+++ colorClass.py
@@ -1,43 +1,49 @@
-class colorPrint:
-    def __init__(self):
+class TheColorPrint:
+    def __init__(self, color=""):
+
+        self.color = color
+        self.White = '\033[97m'
+        self.Purple = '\033[95m'
+        self.Blue = '\033[94m'
+        self.Yellow = '\033[93m'
+        self.Green = '\033[92m'
+        self.Grey = '\033[90m'
+        #
+        self.BOLD = '\033[1m'
+        self.UNDERLINE = '\033[4m'
+
         self.Normal = '\033[0m'
+        self.Red = '\033[91m'
 
-        # White: '\033[97m',
-        # Purple: '\033[95m',
-        # Blue: '\033[94m',
-        # Yellow: '\033[93m',
-        # Green: '\033[92m',
-        # Red: '\033[91m',
-        # Grey: '\033[90m',
-        # Normal: '\033[0m',
-        # BOLD: '\033[1m',
-        # UNDERLINE: '\033[4m',
+    def red(self, text):
+        print(f"{self.Red}{text}{self.Normal}")
 
-        # Clist = {"White": '\033[97m',
-        #          "Purple": '\033[95m',
-        #          "Blue": '\033[94m',
-        #          "Yellow": '\033[93m',
-        #          'Green': '\033[92m',
-        #          "Red": '\033[91m',
-        #          "Grey": '\033[90m',
-        #          "Normal": '\033[0m',
-        #          'BOLD': '\033[1m',
-        #          "UNDERLINE": '\033[4m'}
+    def white(self, text):
+        print(f"{self.White}{text}{self.Normal}")
 
+    def purple(self, text):
+        print(f"{self.Purple}{text}{self.Normal}")
 
-    def printColor(self, color, text):
-        Clist = {"White": '\033[97m',
-                 "Purple": '\033[95m',
-                 "Blue": '\033[94m',
-                 "Yellow": '\033[93m',
-                 'Green': '\033[92m',
-                 "Red": '\033[91m',
-                 "Grey": '\033[90m',
-                 "Normal": '\033[0m',
-                 'BOLD': '\033[1m',
-                 "UNDERLINE": '\033[4m'}
+    def blue(self, text):
+        print(f"{self.Blue}{text}{self.Normal}")
 
-        print(f"{Clist[color]}{text}{self.Normal}")
+    def yellow(self, text):
+        print(f"{self.Yellow}{text}{self.Normal}")
 
-color = colorPrint()
-color.printColor("Green", "asdsd")
+    def green(self, text):
+        print(f"{self.Green}{text}{self.Normal}")
+
+    def grey(self, text):
+        print(f"{self.Grey}{text}{self.Normal}")
+
+    def bold(self, text):
+        print(f"{self.BOLD}{text}{self.Normal}")
+
+    def UNDERLINE(self, text):
+        print(f"{self.UNDERLINE}{text}{self.Normal}")
+
+colorPrint = TheColorPrint("none")
+
+# צריך לעשות אימפורט ל-colorPrint
+# from color class import colorPrint
+# ואז colorPrint נקודה (שם של צבע) וכו'
